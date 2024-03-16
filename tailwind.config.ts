a47a1313bb0ece367451ad68c1f2f6ca0da7be68
mohammad.afshar <mohammad.afshar@partdp.ai>
2024-03-16 07:58:21 +0330

    set default for tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,12 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
+  darkMode: 'class',
   plugins: [],
 }
 export default config
